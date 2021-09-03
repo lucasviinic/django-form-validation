d7c1d7dc08fa6579939a2bbba78f258ba5f260aa
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-03 18:03:53 -0300

    Exibindo dados

--- passagens/views.py
+++ passagens/views.py
@@ -1,7 +1,16 @@
+import passagens
 from django.shortcuts import render
 from passagens.forms import PassagemForms
 
 def index(request):
     form = PassagemForms()
     contexto = {'form': form}
+
     return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {'form': form}
+
+        return render(request, 'minha_consulta.html', contexto)
