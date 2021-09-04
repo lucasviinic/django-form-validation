37018e6fb5cd0438da62ada959ea9497bc2fdaa6
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-04 18:43:31 -0300

    Novos campos

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,9 +2,20 @@ from django import forms
 from django.forms import widgets
 from django.forms.forms import Form
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classe
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today)
+    classe_viagem = forms.ChoiceField(label='Classe do voo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='Email', max_length=150)
